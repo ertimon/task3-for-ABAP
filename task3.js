c2ab0622763387aa272a544fe3df09910c32c4cf
ertimon <ertik1997@mail.ru>
2019-08-05 23:18:03 +0300

    js

---
+++ task3.js
@@ -0,0 +1,45 @@
+'use strict';
+var $ = require("jquery");
+global.DOMParser = require('xmldom').DOMParser;
+require('isomorphic-fetch');
+
+fetch('http://www.cbr.ru/scripts/XML_daily.asp/').then(function(response) {
+  response.text().then(function(text) {
+
+   let parser = new DOMParser();
+   let xmlDoc = parser.parseFromString(text,"text/xml");
+
+   let currency_count = xmlDoc.getElementsByTagName("Valute").length;
+
+   let min = Infinity,
+       max = 0,
+       val = 0,
+       nom = 0,
+       worth = 0,
+       max_i = 0,
+       min_i = 0;
+
+   for (let i = 0; i <= currency_count - 1; i++){
+      nom = Number(xmlDoc.getElementsByTagName("Nominal")[i].childNodes[0].nodeValue);
+      val = xmlDoc.getElementsByTagName("Value")[i].childNodes[0].nodeValue;
+      val = parseFloat(val.replace(',','.'));
+      worth = val / nom;
+        if (worth > max) {
+          max = worth;
+          max_i = i;
+        }
+        if (worth < min){
+          min = worth;
+          min_i = i;
+        }
+   }
+let cheap_curr, expensive_curr;
+cheap_curr = xmlDoc.getElementsByTagName("Name")[min_i].childNodes[0].nodeValue;
+expensive_curr = xmlDoc.getElementsByTagName("Name")[max_i].childNodes[0].nodeValue;
+console.log("cамая дорогая валюта - " + expensive_curr + " курс к рублю - " + min);
+console.log("самая дешевая валюта - " + cheap_curr + " курс к рублю - " + max);
+  }).catch(err => console.error(err));;
+});
+
+
+  
